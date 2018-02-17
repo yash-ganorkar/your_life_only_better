b3095bac41564df20ea7564545f1cbb6e6aee1fc
yash ganorkar <yash.ganorkar@outlook.com>
2018-02-17 09:08:56 -0600

    Your Life Only Better: Commit #1: Initial Commit

---
+++ android/app/src/main/java/com/your_life_only_better/MainActivity.java
@@ -0,0 +1,15 @@
+package com.your_life_only_better;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "your_life_only_better";
+    }
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+import {AppRegistry} from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('your_life_only_better', () => App);
